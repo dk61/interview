4b13afd0a130ee2c8bc5697bb41c39ac9e4e9544
Ilia Denisov <dk61.777@gmail.com>
2024-10-12 12:50:08 +0900

    Renamed OneFrameDummy to OneFrameClient

--- forex-mtl/src/main/scala/forex/services/rates/interpreters/OneFrameClient.scala
+++ forex-mtl/src/main/scala/forex/services/rates/interpreters/OneFrameClient.scala
@@ -7,7 +7,7 @@ import cats.syntax.either._
 import forex.domain.{ Price, Rate, Timestamp }
 import forex.services.rates.errors._
 
-class OneFrameDummy[F[_]: Applicative] extends Algebra[F] {
+class OneFrameClient[F[_]: Applicative] extends Algebra[F] {
 
   override def get(pair: Rate.Pair): F[Error Either Rate] =
     Rate(pair, Price(BigDecimal(100)), Timestamp.now).asRight[Error].pure[F]
