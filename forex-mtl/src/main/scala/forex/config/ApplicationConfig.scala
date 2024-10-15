4e391da4748b21bfb060eddb1858bf222f603d5b
Ilia Denisov <dk61.777@gmail.com>
2024-10-16 02:06:50 +0900

    [NO-JIRA], Added config for provided, simplified access to config for future

--- forex-mtl/src/main/scala/forex/config/ApplicationConfig.scala
+++ forex-mtl/src/main/scala/forex/config/ApplicationConfig.scala
@@ -4,10 +4,17 @@ import scala.concurrent.duration.FiniteDuration
 
 case class ApplicationConfig(
     http: HttpConfig,
+    currencyProvider: CurrencyProviderConfig
 )
 
 case class HttpConfig(
     host: String,
     port: Int,
     timeout: FiniteDuration
+)
+
+case class CurrencyProviderConfig(
+    host: String,
+    port: Int,
+    token: String
 )
