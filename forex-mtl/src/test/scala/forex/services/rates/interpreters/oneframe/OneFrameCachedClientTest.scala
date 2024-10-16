22f24025695b8123965b9fa77054882b92e64152
Ilia Denisov <dk61.777@gmail.com>
2024-10-16 21:03:40 +0900

    [NO-JIRA], fixed tests for negative cases

--- forex-mtl/src/test/scala/forex/services/rates/interpreters/oneframe/OneFrameCachedClientTest.scala
+++ forex-mtl/src/test/scala/forex/services/rates/interpreters/oneframe/OneFrameCachedClientTest.scala
@@ -1,11 +1,12 @@
 package forex.services.rates.interpreters.oneframe
 
-import forex.domain.Currency.{ JPY, USD }
+import forex.domain.Currency.{JPY, USD}
 import forex.domain.Rate.Pair
 import forex.domain.RateOps.PairOps
-import forex.domain.{ Price, Rate, Timestamp }
-import forex.services.rates.errors.Error.{ BadPairProvided, OneFrameLookupFailed }
-import org.mockito.MockitoSugar.{ mock, times, verify, when }
+import forex.domain.{Price, Rate, Timestamp}
+import forex.programs.rates.errors.toProgramError
+import forex.services.rates.errors.Error.{BadPairProvided, OneFrameLookupFailed}
+import org.mockito.MockitoSugar.{mock, times, verify, when}
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 //need to fix when same currency for from and to USD -> USD
@@ -30,8 +31,8 @@ class OneFrameCachedClientTest extends AnyFunSuite with Matchers {
     val cachedClient: OneFrameCachedClient = new OneFrameCachedClient(clientMock)
 
     for {
-      directResponse <- cachedClient.getOneFrameData(pair)
-      oppositeResponse <- cachedClient.getOneFrameData(swappedPair)
+      directResponse <- cachedClient.getOneFrameData(pair).left.map(e => fail(toProgramError(e)))
+      oppositeResponse <- cachedClient.getOneFrameData(swappedPair).left.map(e => fail(toProgramError(e)))
     } yield {
       directResponse shouldBe Rate(pair, Price(0.5), expectedTs)
       oppositeResponse shouldBe Rate(swappedPair, Price(0.5), expectedTs)
@@ -68,8 +69,8 @@ class OneFrameCachedClientTest extends AnyFunSuite with Matchers {
     val cachedClient: OneFrameCachedClient = new OneFrameCachedClient(clientMock)
 
     for {
-      directResponse <- cachedClient.getOneFrameData(pair).swap
-      oppositeResponse <- cachedClient.getOneFrameData(swappedPair).swap
+      directResponse <- cachedClient.getOneFrameData(pair).swap.left.map(r => fail(s"Unexpected rate received $r"))
+      oppositeResponse <- cachedClient.getOneFrameData(swappedPair).swap.left.map(r => fail(s"Unexpected rate received $r"))
     } yield {
       directResponse shouldBe OneFrameLookupFailed(s"Unable to retrieve data for pair = $pair, pls try again later")
       oppositeResponse shouldBe OneFrameLookupFailed(
@@ -94,7 +95,7 @@ class OneFrameCachedClientTest extends AnyFunSuite with Matchers {
     val badPair: Pair                      = Pair(USD, USD)
 
     for {
-      directResponse <- cachedClient.getOneFrameData(badPair).swap
+      directResponse <- cachedClient.getOneFrameData(badPair).swap.left.map(r => fail(s"Unexpected rate received $r"))
     } yield {
       directResponse shouldBe BadPairProvided(s"Pair always should be different, has been provided $badPair")
     }
