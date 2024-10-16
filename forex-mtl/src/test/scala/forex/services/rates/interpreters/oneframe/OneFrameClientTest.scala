30779563e0de2213eb645cf55fff94aeb10dabaf
Ilia Denisov <dk61.777@gmail.com>
2024-10-16 21:03:40 +0900

    [NO-JIRA], fixed tests for negative cases

--- forex-mtl/src/test/scala/forex/services/rates/interpreters/oneframe/OneFrameClientTest.scala
+++ forex-mtl/src/test/scala/forex/services/rates/interpreters/oneframe/OneFrameClientTest.scala
@@ -4,6 +4,7 @@ import cats.effect.{Async, BracketThrow, IO}
 import forex.domain.Currency.{JPY, USD}
 import forex.domain.Rate.Pair
 import forex.domain.{Price, Rate, Timestamp}
+import forex.programs.rates.errors.toProgramError
 import forex.services.rates.errors.Error.OneFrameLookupFailed
 import org.http4s.dsl.Http4sDsl
 import org.scalatest.funsuite.AnyFunSuite
@@ -42,7 +43,7 @@ class OneFrameClientTest extends AnyFunSuite with Http4sDsl[IO] with Matchers {
     OneFrameTestServer.start
       .use { _ =>
         for {
-          response <- oneFrameClient.loadPair(Pair(USD, JPY))
+          response <- oneFrameClient.loadPair(Pair(USD, JPY)).left.map(e => fail(toProgramError(e)))
         } yield {
           response.size shouldBe 2
           response.get(Pair(USD, JPY)) shouldBe Some(
@@ -82,14 +83,47 @@ class OneFrameClientTest extends AnyFunSuite with Http4sDsl[IO] with Matchers {
       .unsafeRunSync()
   }
 
-  test("Test bad data from one frame") {
+  test("Get bad data from one frame") {
+    OneFrameTestServer.response = Ok("""[
+                                       |  {
+                                       |    "from": "USD1",
+                                       |    "to": "JPY1",
+                                       |    "bid": 0.9519282811356304,
+                                       |    "ask": 0.13460390086691998,
+                                       |    "price": 0.54326609100127519,
+                                       |    "time_stamp": "2024-10-15T12:56:50.063Z"
+                                       |  },
+                                       |  {
+                                       |    "from": "JPY",
+                                       |    "to": "USD",
+                                       |    "bid": 0.6588966626132594,
+                                       |    "ask": 0.4033633060536129,
+                                       |    "price": some,
+                                       |    "time_stamp": "2024-10-15T12:56:50.063Z"
+                                       |  }
+                                       |]""".stripMargin)
+    OneFrameTestServer.expectedPairs = List("USDJPY", "JPYUSD")
+
+    OneFrameTestServer.start
+      .use { _ =>
+        for {
+          response <- oneFrameClient.loadPair(Pair(USD, JPY)).swap.left.map(r => fail(s"Unexpected rate received $r"))
+        } yield {
+          response shouldBe OneFrameLookupFailed("Unable to retrieve data for pair = Pair(USD,JPY), pls try again later")
+        }
+        IO.unit
+      }
+      .unsafeRunSync()
+  }
+
+  test("Test empty data from one frame") {
     OneFrameTestServer.response = Ok("[]")
     OneFrameTestServer.expectedPairs = List("USDJPY", "JPYUSD")
 
     OneFrameTestServer.start
       .use { _ =>
         for {
-          response <- oneFrameClient.loadPair(Pair(USD, JPY)).swap
+          response <- oneFrameClient.loadPair(Pair(USD, JPY)).swap.left.map(r => fail(s"Unexpected rate received $r"))
         } yield {
           response shouldBe OneFrameLookupFailed("Unable to process request for pair = Pair(USD,JPY), pls try again later")
         }
@@ -104,7 +138,7 @@ class OneFrameClientTest extends AnyFunSuite with Http4sDsl[IO] with Matchers {
     OneFrameTestServer.start
       .use { _ =>
         for {
-          response <- oneFrameClient.loadPair(Pair(USD, USD)).swap
+          response <- oneFrameClient.loadPair(Pair(USD, USD)).swap.left.map(r => fail(s"Unexpected rate received $r"))
         } yield {
           response shouldBe OneFrameLookupFailed("Unable to process request for pair = Pair(USD,USD), pls try again later")
         }
@@ -119,7 +153,7 @@ class OneFrameClientTest extends AnyFunSuite with Http4sDsl[IO] with Matchers {
     OneFrameTestServer.start
       .use { _ =>
         for {
-          response <- oneFrameClient.loadPair(Pair(USD, JPY)).swap
+          response <- oneFrameClient.loadPair(Pair(USD, JPY)).swap.left.map(r => fail(s"Unexpected rate received $r"))
         } yield {
           response shouldBe OneFrameLookupFailed("Unable to retrieve data for pair = Pair(USD,JPY), pls try again later")
         }
@@ -134,7 +168,7 @@ class OneFrameClientTest extends AnyFunSuite with Http4sDsl[IO] with Matchers {
     OneFrameTestServer.start
       .use { _ =>
         for {
-          response <- oneFrameClient.loadPair(Pair(USD, JPY)).swap
+          response <- oneFrameClient.loadPair(Pair(USD, JPY)).swap.left.map(r => fail(s"Unexpected rate received $r"))
         } yield {
           response shouldBe OneFrameLookupFailed("Unable to retrieve data for pair = Pair(USD,JPY), pls try again later")
         }
